212696230783c6e913bcc5781cdb5c60fd1ddb61
Alex Hodge <hodgea@wesleycollege.net>
2016-04-21 21:00:55 +1000

    Initial commit

---
+++ app/src/main/java/au/com/alexh/carfiles/Entries.java
@@ -0,0 +1,20 @@
+package au.com.alexh.carfiles;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Entries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entries);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
